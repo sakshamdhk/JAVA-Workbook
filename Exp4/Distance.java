d8cfcd65b2e43d178039794b5c7df767b6c6917c
sakshamdhk <24csu189@ncuindia.edu>
2025-09-21 21:40:34 +0530

    Add files via upload

---
+++ Exp4/Distance.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Distance {
+    static double calculate(double x1, double y1, double x2, double y2) {
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter x1: ");
+        double x1 = sc.nextDouble();
+        System.out.print("Enter y1: ");
+        double y1 = sc.nextDouble();
+
+        System.out.print("Enter x2: ");
+        double x2 = sc.nextDouble();
+        System.out.print("Enter y2: ");
+        double y2 = sc.nextDouble();
+
+        double d = calculate(x1, y1, x2, y2);
+        System.out.println("Distance = " + d);
+        System.out.println("Shaksham 24csu357");
+        sc.close();
+}
+}
