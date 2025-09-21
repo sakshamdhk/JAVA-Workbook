aa49f7a737f8327ca615595dd4674e2c649d4de0
sakshamdhk <24csu189@ncuindia.edu>
2025-09-21 21:42:44 +0530

    Update Vehicle.java

--- Exp5/Vehicle.java
+++ Exp5/Vehicle.java
@@ -1,1 +1,44 @@
+import java.util.Scanner;
 
+class Vehicle1 {
+    void display() {
+        System.out.println("This is a Vehicle");
+    }
+}
+
+class Car extends Vehicle1 {
+    void display() {
+        System.out.println("This is a Car");
+    }
+}
+
+class Bike extends Vehicle1 {
+    void display() {
+        System.out.println("This is a Bike");
+    }
+}
+
+class VehicleDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of tyres: ");
+        int N = sc.nextInt();
+
+        Vehicle1 v;
+
+        switch (N) {
+            case 2:
+                v = new Bike();
+                break;
+            case 4:
+                v = new Car();
+                break;
+            default:
+                v = new Vehicle1();
+        }
+
+        v.display();
+        System.out.println("Shaksham 24csu189");
+
+    }
+}
