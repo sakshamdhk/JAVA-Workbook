7ddbd2439007da73f1e38311b95759ef01014dad
sakshamdhk <24csu189@ncuindia.edu>
2025-09-21 21:30:59 +0530

    Update Student.java

--- Exp4/Student.java
+++ Exp4/Student.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+class Student {
+    String name;
+    int roll_no;
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "John";
+        s1.roll_no = 2;
+
+        System.out.println("Name: " + s1.name);
+        System.out.println("Roll No: " + s1.roll_no);
+        System.out.println("Shaksham 24csu189");
+}
+}
